5cb5e103aa0ac65413874c30da0538bbef155f4d
Terri Squire <codeofarmour@outlook.com>
2019-06-02 10:39:32 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,6 +5,12 @@ module Players
       
       if !board.taken?(5)
         move = "5"
+        
+      elsif board.turn_count == 1 
+        move == "1"
+        
+      elsif board.turn_count == 2 
+        move = [1, 3, 7, 9].detect{|i| !board.taken? 
       end 
     end 
   end
